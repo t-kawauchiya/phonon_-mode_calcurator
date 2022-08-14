367cb287249d08d4bbca42b340a2da0c3d8576bb
t-kawauchiya <t.kawauchiya@gmail.com>
2022-08-14 23:47:33 +0900

    ugokuyouninatta!

--- calc_phonon_mode.cpp
+++ calc_phonon_mode.cpp
@@ -1,9 +1,13 @@
+#include "phonon_mode_calculator.h"
+#include <fstream>
 #include <iostream>
-
-// using namespace std;
+#include <string>
 
 int main()
 {
-  std::cout << "HelloWorld." << std::endl;
+  const char *file_name = "input.txt";
+  Phonon_mode_calculator pmc = Phonon_mode_calculator(file_name);
+  pmc.calculate();
+  // pmc.dump();
   return 0;
 }
--- phonon_mode_calculator.h
+++ phonon_mode_calculator.h
@@ -1,9 +1,52 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-class phonon_mode_callurator
+struct Position_vector
+{
+public:
+  double r;
+  double theta;
+  double z;
+  Position_vector(double r, double theta, double z)
+      : r(r), theta(theta), z(z)
+  {
+  }
+};
+struct Phonon_mode
+{
+public:
+  Position_vector displacement;
+  double wave_number;
+  double angular_frequency;
+  Phonon_mode(Position_vector displacement, double wn, double af)
+      : displacement(displacement), wave_number(wn), angular_frequency(af)
+  {
+  }
+};
+struct Material
+{
+public:
+  double elastic_const11;
+  double elastic_const33;
+  // double piezoelectric_const15;
+  // double piezoelectric_const31;
+  // double piezoelectric_const33;
+  Material() {}
+  Material(double ec11, double ec33)
+      : elastic_const11(ec11), elastic_const33(ec33)
+  {
+  }
+};
+class Phonon_mode_calculator
 {
 private:
-  // matarilal
-  // dilat
+  Material material;
+  std::vector<Phonon_mode> dilatational_mode;
+  std::vector<Phonon_mode> torsional_mode;
+
+public:
+  Phonon_mode_calculator(const char *filename);
+  void calculate();
+  void dump();
 };
